02d07bc936dc9ece3774b7b00cc9119c0e64b7bb
Aleksandr Dmitriev <savair88@yandex.ru>
2025-06-17 15:38:43 +0300

    Наброски моделей, сериализаторов и вьюсетов для отзывов, комментариев. (нет роутеров)

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+# User = get_user_model()  # Добавить, , когда появится модель пользователя
+
+
+class ReviewModel(models.Model):
+    text = models.TextField()
+    author = models.IntegerField(null=True)
+    # author = models.ForeignKey(  # Заменить, когда появится модель пользователя
+    #     Users,
+    #     on_delete=models.CASCADE,
+    #     related_name='reviews'
+    # )
+    score = models.IntegerField('Оценка')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        ordering = ('pub_date',)
+
+    def __str__(self):
+        return f'{self.text[:25]} ({self.author=})'
+
+
+class CommentModel(models.Model):
+    text = models.TextField()
+    author = models.IntegerField(null=True)
+    # author = models.ForeignKey(  # Заменить, когда появится модель пользователя
+    #     Users,
+    #     on_delete=models.CASCADE,
+    #     related_name='reviews'
+    # )
+    review = models.ForeignKey(
+        ReviewModel,
+        related_name='comments',
+        on_delete=models.CASCADE
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        ordering = ('pub_date',)
+
+    def __str__(self):
+        return f'{self.text[:25]} ({self.author=})'
--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import ReviewModel, CommentModel
+from api.models import TitleModel
+from .permissions import IsAuthorModeratorAdminOrReadOnly
+from .serializers import ReviewSerializer, CommentSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = ReviewModel.objects.all()
+    serializer_class = ReviewSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (
+        IsAuthenticated,
+        IsAuthorModeratorAdminOrReadOnly,
+    )
+
+    def get_queryset(self):
+        title = get_object_or_404(TitleModel, pk=self.kwargs['title_id'])
+        return title.reviews.all()
+    # У модели TitleModel, если она так называется,
+    # должно быть указано related_name='reviews'
+
+    # def perform_create(self, serializer):
+    #     serializer.save(author=self.request.user)
+    # Добавить, когда появится модель пользователя.
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = CommentModel.objects.all()
+    serializer_class = CommentSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (
+        IsAuthenticated,
+        IsAuthorModeratorAdminOrReadOnly,
+    )
+
+    def get_queryset(self):
+        review = get_object_or_404(ReviewModel, pk=self.kwargs['review_id'])
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(ReviewModel, pk=self.kwargs['review_id'])
+        serializer.save(
+            review=review
+        )
+    # author=self.request.user
+    # Добавить, когда появится модель пользователя.
